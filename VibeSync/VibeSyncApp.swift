4dfa13837e875967eaa267436e52a1efb56d8a4a
Ayyoub <ayyoub@musekits.com>
2026-02-11 15:25:26 -0500

    Initial Commit

---
+++ VibeSync/VibeSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  VibeSyncApp.swift
+//  VibeSync
+//
+//  Created by Ayyoub on 11/2/2026.
+//
+
+import SwiftUI
+
+@main
+struct VibeSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
