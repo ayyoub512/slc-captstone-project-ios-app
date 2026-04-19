2524794764b5ccfe72de4ed9b6d23ff93744bd4c
Ayyoub <ayyoub@musekits.com>
2026-04-19 04:27:19 -0400

    Friend Profile View with stats and remove friend and all conversation history from database and S3 iamges + report user

---
+++ VibeSync/Chat/ViewModels/FriendProfileViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  FriendProfileViewModel.swift
+//  VibeSync
+//
+//  Created by Ayyoub on 19/4/2026.
+//
+
+import Foundation
---
+++ VibeSync/Chat/Views/FriendProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ChatUserProfile.swift
+//  VibeSync
+//
+//  Created by Ayyoub on 19/4/2026.
+//
+
+import SwiftUI
+
+struct ChatUserProfile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatUserProfile()
+}
